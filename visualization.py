0434c69c4fd7bc12f113e91f1f5702ba23f96f97
swirling-wind <yaozirui@outlook.com>
2024-07-19 10:55:33 +0100

    Refactor code to prepare for implementing ray marching

--- visualization.py
+++ visualization.py
@@ -2,37 +2,41 @@ from scene import Scene
 import taichi as ti
 from taichi.math import *
 
+import numpy as np
+from scipy import ndimage
+import trimesh
+import open3d as o3d
 # import sys
 # sys.tracebacklimit=0 # Make error message shorter
 
-scene = Scene()
-scene.set_floor(0, (1, 1, 1))
+# scene = Scene()
+# scene.set_floor(0, (1, 1, 1))
 
-@ti.kernel
-def initialize_voxels():
-    # scene.set_voxel(vec3(10,10,10), 0, vec3(1, 1, 1))
-    for i in range(31):
-        for j in range(31):
-            # Light wall
-            is_light = int(j % 10 != 0)
-            scene.set_voxel(vec3(j, i, -30), is_light + 1, vec3(1, 1, 1))
+# @ti.kernel
+# def initialize_voxels():
+#     # scene.set_voxel(vec3(10,10,10), 0, vec3(1, 1, 1))
+#     for i in range(31):
+#         for j in range(31):
+#             # Light wall
+#             is_light = int(j % 10 != 0)
+#             scene.set_voxel(vec3(j, i, -30), is_light + 1, vec3(1, 1, 1))
 
-            # Rainbow wall
-            color = max(i, j)
-            if color % 2 == 0:
-                scene.set_voxel(
-                    vec3(0, i, j - 30), 1,
-                    vec3((color % 3 // 2) * 0.5 + 0.5,
-                         ((color + 1) % 3 // 2) * 0.5 + 0.5,
-                         ((color + 2) % 3 // 2) * 0.5 + 0.5))
+#             # Rainbow wall
+#             color = max(i, j)
+#             if color % 2 == 0:
+#                 scene.set_voxel(
+#                     vec3(0, i, j - 30), 1,
+#                     vec3((color % 3 // 2) * 0.5 + 0.5,
+#                          ((color + 1) % 3 // 2) * 0.5 + 0.5,
+#                          ((color + 2) % 3 // 2) * 0.5 + 0.5))
 
-    # floor tile
-    for i in range(31):
-        for j in range(31):
-            c = (i + j) % 2
-            index = vec3(i, 0, j - 30)
-            scene.set_voxel(index, 1,
-                            vec3(c * 0.3 + 0.3, (1 - c) * 0.8 + 0.2, 1))
+#     # floor tile
+#     for i in range(31):
+#         for j in range(31):
+#             c = (i + j) % 2
+#             index = vec3(i, 0, j - 30)
+#             scene.set_voxel(index, 1,
+#                             vec3(c * 0.3 + 0.3, (1 - c) * 0.8 + 0.2, 1))
 
-initialize_voxels()
-scene.finish()
+# initialize_voxels()
+# scene.finish()
