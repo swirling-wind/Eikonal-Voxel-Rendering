48cf78e950f72b4ec426d52efef434f6c9acf22e
swirling-wind <yaozirui@outlook.com>
2024-07-29 20:39:01 +0100

    Encapsulate SIREN

--- data/siren.py
+++ data/siren.py
@@ -1,22 +1,12 @@
 import torch
 from torch import nn
-from torch.utils.data import DataLoader, Dataset
-
+from torch.utils.data import DataLoader, Dataset, RandomSampler
 from torchvision.transforms import Resize, Compose, ToTensor, Normalize
 import numpy as np
 from simulation.simulator import DEVICE
 import os
 
 class SineLayer(nn.Module):
-    # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of omega_0.
-
-    # If is_first=True, omega_0 is a frequency factor which simply multiplies the activations before the
-    # nonlinearity. Different signals may require different omega_0 in the first layer - this is a
-    # hyperparameter.
-
-    # If is_first=False, then the weights will be divided by omega_0 so as to keep the magnitude of
-    # activations constant, but boost gradients to the weight matrix (see supplement Sec. 1.5)
-
     def __init__(self, in_features, out_features, bias=True,
                  is_first=False, omega_0=30):
         super().__init__()
@@ -108,13 +98,6 @@ class VoxelFitting(Dataset):
         ])
         irrad_grid = self.transform(voxel_grid)
         self.voxels = irrad_grid.view(1, *sidelength).permute(0, 2, 3, 1).reshape(-1) # type: ignore
-
-    # def __len__(self):
-    #     return 1
-
-    # def __getitem__(self, idx):
-    #     if idx > 0: raise IndexError
-    #     return self.coords, self.voxels
     
     def __len__(self):
         return len(self.coords)
@@ -128,9 +111,7 @@ class ImageFitting(Dataset):
         super().__init__()
         if isinstance(sidelength, int):
             sidelength = (sidelength, sidelength)
-
         self.coords = get_mgrid(sidelength, dim=2)
-
         self.transform = Compose([
             Resize(sidelength),
             ToTensor(),
@@ -143,6 +124,60 @@ class ImageFitting(Dataset):
     def __len__(self):
         return 1
 
-    def __getitem__(self, idx):    
-        
+    def __getitem__(self, idx):
         return self.coords, self.pixels
+    
+class SirenFitter:
+    def __init__(self, input_arr: np.ndarray, floor_heigh: int, sampler_multiplier: int,
+                 hidden_features: int = 256, hidden_layers: int = 3, omega: int = 20):
+        self.floor_height = floor_heigh
+        self.input_arr = input_arr[:, floor_heigh:, :]
+        self.cropped_shape = self.input_arr.shape
+
+        self.siren = Siren(in_features=3, out_features=1, 
+                           hidden_features=hidden_features, hidden_layers=hidden_layers, 
+                           outermost_linear=True,
+                           first_omega_0=omega, hidden_omega_0=omega)
+    
+    def fit(self, total_epochs: int = 20, batch_size: int = 20000, lr: float = 5e-4, patience: int = 5, show_epoch_interval: int = 5):
+        torch.cuda.empty_cache()
+        irrad_data = VoxelFitting(self.input_arr, sidelength=self.input_arr.shape)
+        dataloader = DataLoader(irrad_data, batch_size, sampler=RandomSampler(irrad_data),
+                                pin_memory=True)
+
+        self.siren.cuda()
+        optim = torch.optim.Adam(lr=lr, params=self.siren.parameters())
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, mode='min', factor=0.2, patience=patience)
+
+        self.siren.train()
+        for cur_epoch in range(total_epochs):
+            epoch_loss = 0
+            for model_input, ground_truth in dataloader:
+                model_input, ground_truth = model_input.cuda(), ground_truth.cuda()
+                model_output = self.siren(model_input)
+                model_output = model_output.view(ground_truth.shape)
+                loss = ((model_output - ground_truth) ** 2).mean()
+                epoch_loss += loss.item()
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+
+            epoch_loss /= len(dataloader)
+            scheduler.step(epoch_loss)
+
+            if cur_epoch == total_epochs - 1 or cur_epoch == 0 or (cur_epoch+1) % show_epoch_interval == 0:
+                print("Epoch: {} \tAverage Loss: {:.4f} \tLearning Rate: {}".format(cur_epoch + 1, epoch_loss,
+                                                                                    scheduler.get_last_lr()))
+
+    def infer(self, pad: bool = True) -> np.ndarray:
+        torch.cuda.empty_cache()
+        infer_coord = get_mgrid(self.cropped_shape, dim=3).cuda()
+        self.siren.eval()
+        with torch.no_grad():
+            infer_output: torch.Tensor = self.siren(infer_coord).view(self.cropped_shape).detach()
+        
+        if pad:
+            padding = (0, 0, self.floor_height, 0, 0, 0) 
+            infer_output = torch.nn.functional.pad(infer_output, padding, mode='constant', value=0)
+        
+        return infer_output.cpu().numpy()
